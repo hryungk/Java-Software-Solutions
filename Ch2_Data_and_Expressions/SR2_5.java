38122170fc6b6376308ec33e7e7f12fc08ad62d2
hryungk <38110685+hryungk@users.noreply.github.com>
2020-07-08 12:02:31 -0700

    Add files via upload

---
+++ Ch2_Data_and_Expressions/SR2_5.java
@@ -0,0 +1,22 @@
+//********************************************************************************
+//  SR2_5.java      @author: Hyunryung Kim
+//
+//  Outputs the answer for the Self-review question 2.5
+//********************************************************************************
+
+public class SR2_5 
+{
+    //----------------------------------------------------------------------------
+    //  What output is produced by the following code fragment?
+    //----------------------------------------------------------------------------
+    public static void main(String[] args) 
+    {
+        System.out.print("Ready ");
+        System.out.println();
+        System.out.println("Set ");
+        System.out.println();
+        System.out.println("Go ");
+        
+    }
+    
+}
---
+++ Ch2_Data_and_Expressions/SR2_8.java
@@ -0,0 +1,20 @@
+//********************************************************************************
+//  SR2_8.java      @author: Hyunryung Kim
+//
+//  Outputs the answer for the Self-review question 2.8
+//********************************************************************************
+
+public class SR2_8 
+{
+    //----------------------------------------------------------------------------
+    //  Write a single println statement that will output the following exactly 
+    //  as shown (including line breakes and quation marks).
+    //----------------------------------------------------------------------------
+    public static void main(String[] args) 
+    {
+        System.out.print(" \"I made this letter longer than usual\nbecause "
+                + "I lack the time to make it short.\"\n\tBlaise Pascal\n");
+        
+    }
+    
+}
